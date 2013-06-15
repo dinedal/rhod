7487d19f77c7590f4733fee5f044a999bcd5f4a9
Paul Bergeron <paul.d.bergeron@gmail.com>
2013-06-15 14:13:30 -0700

    Middleware with hooks approach started

--- lib/rhod/middleware.rb
+++ lib/rhod/middleware.rb
@@ -1,9 +1,63 @@
 class Rhod::Middleware
+  class Rhod::InvalidMiddleware < Exception; end
+
+  def initialize
+  end
+
   def use(middleware, *args, &block)
-    self.stack << [middleware, args, block]
+    stack << [middleware, args, block]
+  end
+
+  def build_stack
+    stack = stack.map do |current_middleware|
+      klass, args, block = current_middleware
+
+      if klass.is_a?(Class)
+        klass.new(*args, &block)
+      else
+        raise Rhod::InvalidMiddleware, "Unable to call middleware #{current_middleware}"
+      end
+    end
+  end
+
+  def on_before(env)
+    stack.reduce(env) do |e, current_middleware|
+      if current_middleware.respond_to?(:before)
+        e = current_middleware.before(e)
+      end
+      e
+    end
+  end
+
+  def on_after(env)
+    stack.reduce(env) do |e, current_middleware|
+      if current_middleware.respond_to?(:after)
+        e = current_middleware.after(e)
+      end
+      e
+    end
+  end
+
+  def on_error(env)
+    stack.reduce(env) do |e, current_middleware|
+      if current_middleware.respond_to?(:error)
+        e = current_middleware.error(e)
+      end
+      e
+    end
+  end
+
+  def on_failure(env)
+    stack.reduce(env) do |e, current_middleware|
+      if current_middleware.respond_to?(:failure)
+        e = current_middleware.failure(e)
+      end
+      e
+    end
   end
 
   protected
+
   def stack
     @stack ||= []
   end
